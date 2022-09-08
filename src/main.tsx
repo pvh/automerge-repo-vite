17f3aec30278691b51a0d4295aa488dbf4ede895
Peter van Hardenberg <pvh@pvh.ca>
2022-09-08 13:54:15 -0700

    broken app but for investigation

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import {
+  // @ts-expect-error
+  BrowserRepo,
+  // @ts-expect-error
+  LocalForageStorageAdapter,
+  // @ts-expect-error
+  BroadcastChannelNetworkAdapter,
+  // @ts-expect-error
+  BrowserWebSocketClientAdapter,
+  Repo,
+} from "automerge-repo"
+import { RepoContext } from './hooks'
+
+async function getRepo(url: string) {
+  return await BrowserRepo({
+    storage: new LocalForageStorageAdapter(),
+    network: [
+      new BroadcastChannelNetworkAdapter(),
+      // new BrowserWebSocketClientAdapter(url)
+    ],
+  })
+}
+
+const params = new URLSearchParams(window.location.search);
+const hostname = params.get("host") || "automerge-storage-demo.glitch.me"
+
+getRepo(`wss://${hostname}`).then( repo => ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RepoContext.Provider value={repo}>
+      <App />
+    </RepoContext.Provider>
   </React.StrictMode>
-)
+))
